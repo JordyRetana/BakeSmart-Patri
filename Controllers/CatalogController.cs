91f268d46ef891f847e3b30573418cc677e3f9c8
Jordy Retana <jretanamendez@gmail.com>
2026-08-09 16:35:18 -0600

    Add client catalog cart flow

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -25,6 +25,11 @@ namespace BakeSmartPatri.Controllers
             return View(await BuildIndexModelAsync());
         }
 
+        public async Task<IActionResult> Cart()
+        {
+            return View(await BuildIndexModelAsync());
+        }
+
         public IActionResult Categories() => RedirectToAction(nameof(Index), new { categories = "open" });
 
         public async Task<IActionResult> Offers() => View(await BuildIndexModelAsync());
